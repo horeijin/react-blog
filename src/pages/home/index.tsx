33e2540aa9f62fa9ad6dd7aa88ef6de51ffb3916
myMac <dgbuzzer@gmail.com>
2023-12-08 16:46:29 +0900

    feature: home 페이지 작성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,11 +13,33 @@ export const Home: FC<Props> = () => {
           <Link to="/profile">Profile</Link>
         </div>
       </header>
-      <div className="post-list">Posts List</div>
+      <div className="post__navigation">
+        <div className="post__navigation-active">All</div>
+        <div>My Article</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, i) => (
+          <div key={i} className="post__box">
+            <Link to={`/posts/${i}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">작성자</div>
+                <div className="post__date">작성일</div>
+              </div>
+              <div className="post__title">게시글 {i}</div>
+              <div className="post__text">작성 내용</div>
+              <div className="post__utils-box">
+                <div className="post__delete">delete</div>
+                <div className="post__edit">edit</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <li>menu 1</li>
-        <li>menu 2</li>
-        <li>menu 3</li>
+        <Link to="/posts/new">Write</Link>
+        <Link to="/posts">List</Link>
+        <Link to="/profile">Profile</Link>
       </footer>
     </div>
   );
