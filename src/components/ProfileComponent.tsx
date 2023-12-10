f580272e7303063a5451089bbcc3cdfdb09cc67b
myMac <dgbuzzer@gmail.com>
2023-12-10 16:56:09 +0900

    contextAPI로 user 상태관리

--- src/components/ProfileComponent.tsx
+++ src/components/ProfileComponent.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
 import { toast } from "react-toastify";
+import AuthContext from "context/AuthContext";
 
 interface Props {}
 
@@ -16,16 +17,15 @@ const onSignOut = async () => {
 };
 
 export const ProfileComponent: FC<Props> = () => {
-  const auth = getAuth(app);
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">
-            {auth?.currentUser?.displayName || "user"}
-          </div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || "user"}</div>
         </div>
       </div>
       <div role="presentation" className="profile__logout" onClick={onSignOut}>
